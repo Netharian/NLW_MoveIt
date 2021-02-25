156ef2c6fca8030c862ec8071a19610c2a252a6c
Lucas Melo <lucasmfs21@gmail.com>
2021-02-25 11:33:10 -0300

    ???

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
@@ -8,15 +7,6 @@ export function ExperienceBar() {
             <div>
                 <div style={{ width: '50%' }} />
                 <span className={styles.currentExperience} style={{ left: "50%" }}>300 xp</span>
-=======
-export function ExperienceBar() {
-    return (
-        <header className="experience-bar">
-            <span>0 xp</span>
-            <div>
-                <div style={{ width: '50%' }} />
-                <span className="current-experience" style={{ left: "50%" }}>300 xp</span>
->>>>>>> 56e9748da9092b52554ec327ab2b0bebd74dc8aa
             </div>
             <span>600 xp</span>
         </header>
